fc9b1723f2a728732ef015d5963a942e639a626e
Thomas Richards <Deskbot@users.noreply.github.com>
2023-08-31 22:54:20 +0100

    spacing

--- src/component/Main.ts
+++ src/component/Main.ts
@@ -1,5 +1,4 @@
-import { div, h1, main } from "ariamis";
-import { AddTimerButton } from "./AddTimerButton";
+import { h1, main } from "ariamis";
 import { ButtonBar } from "./ButtonBar";
 import "./Main.css";
 import { Timers } from "./Timers";
@@ -9,9 +8,7 @@ export function Main() {
     return main([
         h1(["Time tracker"]),
         TotalTime(),
-        div(),
         Timers(),
-        div(),
         ButtonBar(),
     ])
 }

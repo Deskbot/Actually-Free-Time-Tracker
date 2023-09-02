8f9363f5fbee51691a7b06e0a503678e998b61a3
Thomas Richards <Deskbot@users.noreply.github.com>
2023-09-02 23:15:58 +0100

    graph styling

--- src/component/Graph.ts
+++ src/component/Graph.ts
@@ -4,6 +4,7 @@ import { Timer } from "../domain/Timer";
 import { joinObservables, mapObservableArray } from "../observable/observable";
 import { highestTimerMilliseconds, timers } from "../state/timerState";
 import "./Graph.css";
+import { flexGrow } from "../dom/flex";
 
 export function Graph() {
     return div({ className: "graph" }, [
@@ -26,8 +27,9 @@ function Bar(timer: Timer) {
     )
 
     const elem = div({ className: "bar" }, [
-        fromObservable(timer.name, name => span({ title: name }, [name])),
-        fromObservable(percent, p => span([p + "%"]))
+        fromObservable(percent, p => div({ className: "percentage" }, [p.toFixed(0) + "%"])),
+        flexGrow(),
+        fromObservable(timer.name, name => div({ className: "name", title: name }, [name])),
     ])
 
     percent.onChange(p => elem.style.height = (p * 100) + "%", true)

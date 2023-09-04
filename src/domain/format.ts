65e89b68f1e772226ba3f46bd64ba7959157db0c
Thomas Richards <Deskbot@users.noreply.github.com>
2023-09-05 00:08:16 +0100

    change formatting

--- src/domain/format.ts
+++ src/domain/format.ts
@@ -2,23 +2,27 @@
 export function formatTime(milliseconds: number) {
     const seconds = Math.floor(milliseconds / 1000)
     if (seconds < 60) {
-        return formatNum(seconds) + " seconds"
+        return align(formatNum(seconds) + "s")
     }
 
     const minutes = Math.floor(seconds / 60)
-    const minutesStr = formatNum(minutes) + " minutes"
+    const andSeconds = seconds % 60
 
+    const minutesStr = formatNum(minutes) + "m"
     if (minutes < 10) {
-        const andSeconds = seconds % 60
-        return minutesStr + " " + formatNum(andSeconds) + " seconds"
+        return align(minutesStr + " " + formatNum(andSeconds) + "s")
     }
     if (minutes < 60) {
-        return minutesStr
+        return align(minutesStr)
     }
 
     const hours = Math.floor(minutes / 60)
     const andMinutes = minutes % 60
-    return formatNum(hours) + " hours " + formatNum(andMinutes) + " minutes"
+    return align(formatNum(hours) + "h " + formatNum(andMinutes) + "m " + formatNum(andSeconds) + "s")
+}
+
+function align(str: string) {
+    return str.padStart(10)
 }
 
 function formatNum(n: number): string {
